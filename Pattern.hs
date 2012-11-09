aff3ba99189954775e0a07ca8aead4e5f2c0905e
Jon Andersen <jon.andersen88@gmail.com>
2012-11-09 16:29:57 +0100

    Implemented matching

--- Pattern.hs
+++ Pattern.hs
@@ -1,6 +1,6 @@
 module Pattern where
 import Utilities
-
+import Maybe
 
 -------------------------------------------------------
 -- Match and substitute
@@ -8,26 +8,33 @@ import Utilities
 
 -- Replaces a wildcard in a list with the list given as the third argument
 substitute :: Eq a => a -> [a] -> [a] -> [a]
-substitute _ ys [] = ys 
+substitute _ xs [] = xs 
 substitute _ [] _ = []
-substitute x (y:ys) zs 
-  | x == y    = zs ++ (substitute x ys zs)
-  | otherwise = y:(substitute x ys zs)
+substitute wc (x:xs) ys 
+  | wc == x    = ys ++ (substitute wc xs ys)
+  | otherwise = x:(substitute wc xs ys)
 
 
 -- Tries to match two lists. If they match, the result consists of the sublist
 -- bound to the wildcard in the pattern list.
 match :: Eq a => a -> [a] -> [a] -> Maybe [a]
-match _ _ _ = Nothing
-{- TO BE WRITTEN -}
+match _ [] [] = Just []
+match _ [] _ = Nothing
+match _ _ [] = Nothing
+match wc (p:ps) (x:xs) 
+  | x == p    = match wc ps xs
+  | wc == p   = singleWildcardMatch (p:ps) (x:xs) `orElse` longerWildcardMatch (p:ps) (x:xs)
+  | otherwise = Nothing
 
 
 -- Helper function to match
 singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
-singleWildcardMatch (wc:ps) (x:xs) = Nothing
-{- TO BE WRITTEN -}
-longerWildcardMatch (wc:ps) (x:xs) = Nothing
-{- TO BE WRITTEN -}
+-- singleWildcardMatch (wc:ps) (x:xs) = mmap (const [x]) (match wc ps xs)  
+singleWildcardMatch (wc:ps) (x:xs) 
+  | isJust $ match wc ps xs = Just [x]
+  | otherwise = Nothing
+longerWildcardMatch (wc:ps) (x:xs) = mmap (x:) (match wc (wc:ps) xs)
+
 
 
 
